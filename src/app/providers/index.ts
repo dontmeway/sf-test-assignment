cb8cc10bad2735d37873284364ac6794944c2989
dontmeway <bbakhadyrov@gmail.com>
2022-10-12 16:45:43 +0500

    fix: compose fn

--- src/app/providers/index.ts
+++ src/app/providers/index.ts
@@ -1,12 +1,4 @@
 import { withRouter } from './with-router'
+import compose from 'compose-function'
 
-export const withProviders = composeHoc(withRouter)
-
-function composeHoc<T>(...fns: ((node: T) => T)[]) {
-  return (node: T) => {
-    return fns.reduceRight((acc, next) => {
-      acc = next(acc)
-      return acc
-    }, node)
-  }
-}
+export const withProviders = compose(withRouter)
